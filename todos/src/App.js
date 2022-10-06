fa175a39dfe50ca94fd3e11edcc812d78f6880a5
sneha shinde <Sneha06032000@gmail.com>
2022-10-07 00:13:19 +0530

    practice

---
+++ todos/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css'
+// import { CountClass } from "./components/CountClass";
+// import { CountHooks } from "./components/CountHooks";
+// import { CounterHooks } from "./components/CounterHooks";
+// import { DataFetch } from "./components/DataFetch";
+// import { ReducerCounter } from "./components/ReducerCounter";
+// import { TitleChangeClass } from "./components/TitleChangeClass";
+// import { TitleChangeHooks } from "./components/TitleChangeHooks";
+import  Check  from "./components/Check";
+
+
+
+function App() {
+  return (
+    <>
+      {/* <CountClass />
+      <CountHooks />
+      <CounterHooks />
+      <DataFetch /> 
+      <ReducerCounter/> */}
+      {/* <TitleChangeClass /> */}
+      {/* <TitleChangeHooks /> */}
+      <Check />
+    </>
+  );
+}
+
+export default App;
